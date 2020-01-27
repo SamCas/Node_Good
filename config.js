2149766d580148fa1576a3a1db680e9f59891638
Samuel Castillo <samuel.cas.96@gmail.com>
2020-01-27 08:09:46 -0600

    Mod

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/university";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://SamCas:Qpwoeiruty123890@prograweb-zubj1.mongodb.net/university?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
